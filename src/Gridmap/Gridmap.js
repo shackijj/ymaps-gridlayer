5366f51fc2c1df76f860cec96e0392e46871e18a
Kirill Shatskiy <shaackiy@gmail.com>
2018-05-12 18:51:05 +0300

    Create prototype.

--- src/Gridmap/Gridmap.js
+++ src/Gridmap/Gridmap.js
@@ -1,13 +1,17 @@
 import {getHexagonsForTile} from './utils/hexagon';
+import RTree from 'rtree';
 
 ymaps.modules.define('Gridmap', [
     'Layer',
-    'util.hd'
-], (provide, Layer, utilHd) => {
+    'util.hd',
+    'util.defineClass',
+    'util.extend'
+], (provide, Layer, utilHd, defineClass, extend) => {
     const TILE_SIZE = 256;
     const dpr = utilHd.getPixelRatio();
+    const ZOOM = 10;
+    const R = 15;
 
-    const R = 50;
     function sin(angle) {
         return Math.sin(Math.PI * angle / 180);
     }
@@ -16,21 +20,103 @@ ymaps.modules.define('Gridmap', [
         return Math.cos(Math.PI * angle / 180);
     }
     class Canvas {
-        constructor(size) {
+        constructor(size, {features}, map) {
+            this._projection = map.options.get('projection');
+            this._data = features;
             this._canvas = document.createElement('canvas');
             this._canvas.width = size[0] * dpr;
             this._canvas.height = size[1] * dpr;
             this._context = this._canvas.getContext('2d');
+            this._buildTree();
         }
 
+        _buildTree() {
+            this._tree = new RTree();
+            this._data.forEach((feature) => {
+                const [x, y] = this._projection.toGlobalPixels(
+                    feature.geometry.coordinates, ZOOM);
+                this._tree.insert(
+                    {
+                        x,
+                        y,
+                        w: 0,
+                        h: 0
+                    },
+                    feature);
+            });
+        }
         _clear() {
             this._context.clearRect(0, 0, this._canvas.width, this._canvas.height);
+        }
+
+        _getShapesForTile(tileNumber, zoom, R) {
+            const hexogons = getHexagonsForTile(tileNumber, TILE_SIZE, R);
+            const offset = [
+                tileNumber[0] * TILE_SIZE,
+                tileNumber[1] * TILE_SIZE
+            ];
+            return hexogons.map(([x, y]) => {
+                const globalBbox = {
+                    x: (x + offset[0]) - R,
+                    y: (y + offset[1]) - R,
+                    w: R,
+                    h: R
+                };
+                const points = this._tree.search(globalBbox);
+
+                const left = x - R;
+                const top = y - R;
+
+                return {
+                    type: 'Feature',
+                    properties: {
+                        hintContent: 'Содержимое текстовой подсказки.',
+                        balloonContentBody: `Тут ${points.length} точек!`,
+                        balloonContentHeader: 'Заголовок балуна.',
+                        balloonContentFooter: 'Нижняя часть балуна.',
+                        // Можно задавать свойство balloonContent вместо Body/Header/Footer
+
+                        // Обязательное поле
+                        HotspotMetaData: {
+                            // Идентификатор активной области.
+                            id: Date.now(),
+
+                            // Данные, на основе которых создается геометрия активной области.
+                            // Обязательное поле.
+                            RenderedGeometry: {
+                                type: 'Polygon',
+                                // Координаты многоугольника.
+                                coordinates: [
+                                    [
+                                        [left, top],
+                                        [left + 2 * R, top],
+                                        [left + 2 * R, top + 2 * R],
+                                        [left, top + 2 * R],
+                                        [top, left]
+                                    ]
+                                ]
+                            }
+                        }
+                    }
+                };
+            });
         }
 
         _drawTile(tileNumber) {
             this._clear();
             const hexogons = getHexagonsForTile(tileNumber, TILE_SIZE, R);
+            const offset = [
+                tileNumber[0] * TILE_SIZE,
+                tileNumber[1] * TILE_SIZE
+            ];
             hexogons.forEach(([x, y]) => {
+                const globalBbox = {
+                    x: (x + offset[0]) - R,
+                    y: (y + offset[1]) - R,
+                    w: R,
+                    h: R
+                };
+                const points = this._tree.search(globalBbox);
                 const hexagon = [
                     [cos(0), sin(0)],
                     [cos(60), sin(60)],
@@ -49,6 +135,9 @@ ymaps.modules.define('Gridmap', [
                         this._context.lineTo(x * R * dpr, y * R * dpr);
                     }
                 });
+                const ratio = points.length / this._data.length;
+                this._context.fillStyle = `rgba(0,255,0,${ratio * 100})`;
+                this._context.fill();
                 this._context.stroke();
                 this._context.closePath();
                 this._context.setTransform(1, 0, 0, 1, 0, 0);
@@ -61,14 +150,30 @@ ymaps.modules.define('Gridmap', [
         }
     }
 
-    const canvas = new Canvas([256, 256]);
-
-    function tileUrlTemplate(tileNumber, tileZoom) {
-        return canvas.getDataURL(tileNumber, tileZoom);
-    }
-
     class Gridmap {
-        constructor(map) {
+        constructor(map, featureCollection) {
+            const canvas = new Canvas([256, 256], featureCollection, map);
+
+            function tileUrlTemplate(tileNumber, tileZoom) {
+                return canvas.getDataURL(tileNumber, tileZoom);
+            }
+
+            const HotspotLayerLocal = function (data, options) {
+                HotspotLayerLocal.superclass.constructor.call(this, '', extend({}, options));
+            };
+
+            defineClass(HotspotLayerLocal, ymaps.hotspot.ObjectSource, {
+                requestObjects: function (layer, tileNumber, zoom, callback) {
+                    const features = {
+                        data: {
+                            type: 'FeatureCollection',
+                            features: canvas._getShapesForTile(tileNumber, ZOOM, R)
+                        }
+                    };
+                    this.parseResponse(layer, features, callback, tileNumber, ZOOM);
+                }
+            });
+
             const layer = new ymaps.Layer(tileUrlTemplate, {
                 /**
                  * This is necessary because otherwise tiles are rendered
@@ -76,6 +181,9 @@ ymaps.modules.define('Gridmap', [
                  */
                 tileTransparent: true
             });
+            const objSource = new HotspotLayerLocal(tileUrlTemplate);
+            const hotspotLayer = new ymaps.hotspot.Layer(objSource, {zIndex: 2000, cursor: 'help'});
+            map.layers.add(hotspotLayer);
             map.layers.add(layer);
         }
 
--- src/examples/index.js
+++ src/examples/index.js
@@ -1,15 +1,16 @@
 import './../Gridmap/Gridmap';
+import data from './data-latlong.geojson';
 
 ymaps.ready(() => {
     // eslint-disable-next-line no-unused-vars
     const myMap = new ymaps.Map('map', {
         center: [55.76, 37.64],
-        zoom: 0,
+        zoom: 10,
         controls: ['zoomControl', 'typeSelector', 'fullscreenControl']
     });
 
     ymaps.modules.require(['Gridmap'], (Gridmap) => {
-        const data = [[55.76, 37.64], [55.76, 37.65]];
+        // eslint-disable-next-line
         const gridmap = new Gridmap(myMap, data);
     });
 });

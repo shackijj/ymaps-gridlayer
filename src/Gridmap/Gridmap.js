7ae2fa9fd738b21604fe33da6e30a62de4292779
Kirill Shatskiy <shaackiy@gmail.com>
2018-05-17 10:40:43 +0300

    Move the common code into functions.

--- src/Gridmap/Gridmap.js
+++ src/Gridmap/Gridmap.js
@@ -1,5 +1,6 @@
 import {getCentersOfHexagonsForTile} from './utils/hexagon';
 import RTree from 'rtree';
+import classifyPoint from 'robust-point-in-polygon';
 
 ymaps.modules.define('Gridmap', [
     'Layer',
@@ -10,7 +11,7 @@ ymaps.modules.define('Gridmap', [
     const TILE_SIZE = 256;
     const dpr = utilHd.getPixelRatio();
     const ZOOM = 10;
-    const R = 50;
+    const R = 15;
     const r = sin(60) * R;
 
     function sin(angle) {
@@ -36,8 +37,10 @@ ymaps.modules.define('Gridmap', [
             this._data.forEach((feature) => {
                 const [x, y] = this._projection.toGlobalPixels(
                     feature.geometry.coordinates, ZOOM);
-                feature.properties.RenderedGeometry = {
-                    coordinates: [x, y]
+
+                const point = {
+                    feature,
+                    pixelCoords: [x, y]
                 };
                 this._tree.insert(
                     {
@@ -46,83 +49,99 @@ ymaps.modules.define('Gridmap', [
                         w: 0,
                         h: 0
                     },
-                    feature);
+                    point);
             });
         }
         _clear() {
             this._context.clearRect(0, 0, this._canvas.width, this._canvas.height);
         }
 
-        _getShapesForTile(tileNumber, zoom, R) {
-            const hexogons = getCentersOfHexagonsForTile(tileNumber, TILE_SIZE, R);
-            const offset = [
-                tileNumber[0] * TILE_SIZE,
-                tileNumber[1] * TILE_SIZE
+        _getPointsForShape(shapeCenter, globalOffset, R, r) {
+            const [x, y] = shapeCenter;
+
+            const globalBbox = {
+                x: (x + globalOffset[0]) - R,
+                y: (y + globalOffset[1]) - r,
+                w: 2 * R,
+                h: 2 * r
+            };
+            const shape = this._getShapeGlobal(shapeCenter, globalOffset, R);
+            const points = this._tree
+                .search(globalBbox)
+                .filter(({pixelCoords}) => classifyPoint(shape, pixelCoords) >= 0);
+
+            return points;
+        }
+
+        _getShapeGlobal(shapeCenter, globalOffset, R) {
+            const [x, y] = shapeCenter;
+            const hexagon = [
+                [cos(0), sin(0)],
+                [cos(60), sin(60)],
+                [cos(120), sin(120)],
+                [cos(180), sin(180)],
+                [cos(240), sin(240)],
+                [cos(300), sin(300)],
+                [cos(0), sin(0)]
             ];
-            return hexogons.map(([x, y]) => {
-                const globalBbox = {
-                    x: (x + offset[0]) - R,
-                    y: (y + offset[1]) - r,
-                    w: 2 * R,
-                    h: 2 * r
-                };
-                const points = this._tree.search(globalBbox);
 
-                const hexagon = [
-                    [cos(0), sin(0)],
-                    [cos(60), sin(60)],
-                    [cos(120), sin(120)],
-                    [cos(180), sin(180)],
-                    [cos(240), sin(240)],
-                    [cos(300), sin(300)],
-                    [cos(0), sin(0)]
-                ];
+            return hexagon.map(([hX, hY]) => [x + globalOffset[0] + (hX * R), y + globalOffset[1] + (hY * R)]);
+        }
 
-                const hexagon2d = hexagon.map(([hX, hY]) => [x + (hX * R), y + (hY * R)]);
-                return {
-                    type: 'Feature',
-                    properties: {
-                        balloonContentBody: `Тут ${points.length} точек!`,
-                        balloonContentHeader: JSON.stringify([x, y]),
-                        balloonContentFooter: 'Нижняя часть балуна.',
-                        // Можно задавать свойство balloonContent вместо Body/Header/Footer
+        _getTileOffset(tileNumber, tileSize) {
+            return [
+                tileNumber[0] * tileSize,
+                tileNumber[1] * tileSize
+            ];
+        }
 
-                        // Обязательное поле
-                        HotspotMetaData: {
-                            // Идентификатор активной области.
-                            id: Date.now(),
+        _getHotspotsForTile(tileNumber, zoom, R) {
+            const hexogons = getCentersOfHexagonsForTile(tileNumber, TILE_SIZE, R);
+            const offset = this._getTileOffset(tileNumber, TILE_SIZE);
+            const result = [];
+            hexogons.forEach(([x, y]) => {
+                const points = this._getPointsForShape([x, y], offset, R, r);
+                if (points.length > 0) {
+                    const hexagon = this._getShapeGlobal([x, y], [0, 0], R);
+                    result.push({
+                        type: 'Feature',
+                        properties: {
+                            balloonContentBody: `Тут ${points.length} точек!`,
+                            balloonContentHeader: JSON.stringify([x, y]),
+                            balloonContentFooter: 'Нижняя часть балуна.',
+                            // Можно задавать свойство balloonContent вместо Body/Header/Footer
 
-                            // Данные, на основе которых создается геометрия активной области.
-                            // Обязательное поле.
-                            RenderedGeometry: {
-                                type: 'Polygon',
-                                // Координаты многоугольника.
-                                coordinates: [
-                                    hexagon2d
-                                ]
+                            // Обязательное поле
+                            HotspotMetaData: {
+                                // Идентификатор активной области.
+                                id: JSON.stringify(points),
+                                // Данные, на основе которых создается геометрия активной области.
+                                // Обязательное поле.
+                                RenderedGeometry: {
+                                    type: 'Polygon',
+                                    // Координаты многоугольника.
+                                    coordinates: [
+                                        hexagon
+                                    ]
+                                }
                             }
                         }
-                    }
-                };
+                    });
+                }
             });
+
+            return result;
         }
 
         _drawTile(tileNumber) {
             this._clear();
             this._context.strokeRect(0, 0, TILE_SIZE * dpr, TILE_SIZE * dpr);
+
             const hexogons = getCentersOfHexagonsForTile(tileNumber, TILE_SIZE, R);
-            const offset = [
-                tileNumber[0] * TILE_SIZE,
-                tileNumber[1] * TILE_SIZE
-            ];
+            const offset = this._getTileOffset(tileNumber, TILE_SIZE);
+
             hexogons.forEach(([x, y]) => {
-                const globalBbox = {
-                    x: (x + offset[0]) - R,
-                    y: (y + offset[1]) - r,
-                    w: 2 * R,
-                    h: 2 * r
-                };
-                const points = this._tree.search(globalBbox);
+                const points = this._getPointsForShape([x, y], offset, R, r);
                 const hexagon = [
                     [cos(0), sin(0)],
                     [cos(60), sin(60)],
@@ -142,13 +161,11 @@ ymaps.modules.define('Gridmap', [
                     }
                 });
                 const ratio = points.length / this._data.length;
-                this._context.fillStyle = `rgba(0,255,0,${ratio * 50})`;
+                this._context.fillStyle = `rgba(0,255,0,${ratio * 100})`;
                 this._context.fill();
                 this._context.stroke();
                 this._context.closePath();
                 this._context.setTransform(1, 0, 0, 1, 0, 0);
-                this._context.fillStyle = 'black';
-                this._context.fillText('(' + x + ' ' + y + ')', x * dpr, y * dpr);
             });
         }
 
@@ -175,7 +192,7 @@ ymaps.modules.define('Gridmap', [
                     const features = {
                         data: {
                             type: 'FeatureCollection',
-                            features: canvas._getShapesForTile(tileNumber, ZOOM, R)
+                            features: canvas._getHotspotsForTile(tileNumber, ZOOM, R)
                         }
                     };
                     this.parseResponse(layer, features, callback, tileNumber, ZOOM);

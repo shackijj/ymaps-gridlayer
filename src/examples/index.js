2bb8291b52c71fb06edd3d12a82269d741a31d10
Kirill Shatskiy <shaackiy@gmail.com>
2018-05-18 16:59:55 +0300

    Update examples.

--- src/examples/index.js
+++ src/examples/index.js
@@ -1,5 +1,5 @@
 import './../Gridmap/Gridmap';
-import data from './data/cameras.json';
+import data from './data/bikeparking.json';
 
 ymaps.ready(() => {
     // eslint-disable-next-line no-unused-vars

94cc7ffb6c02a517a168deac290dd11ff70f30b8
Denis Tokarev <blok1993@yandex.ru>
2018-05-19 18:43:25 +0300

    feat: interactivity added

---
+++ src/Gridmap/utils/defaultOnMouseEnter.js
@@ -0,0 +1,20 @@
+/**
+ * Default handler for mouseEnter event.
+ *
+ * @param {Object} e Event object.
+ * @this Gridmap
+ */
+const defaultOnMouseEnter = function (e) {
+    const activeShape = e.originalEvent.activeObject;
+
+    this.polygonHover = new ymaps.Polygon([activeShape._properties.objectGeometry], {
+        hintContent: 'Polygon'
+    }, {
+        fillColor: '#00FF0088',
+        strokeWidth: 3
+    });
+
+    this._options.map.geoObjects.add(this.polygonHover);
+};
+
+export default defaultOnMouseEnter;

aa83c6dc0d6c771155a6c6284615c85538a55c16
Denis Tokarev <blok1993@yandex.ru>
2018-05-19 18:43:25 +0300

    feat: interactivity added

---
+++ src/Gridmap/utils/defaultOnClick.js
@@ -0,0 +1,20 @@
+/**
+ * Default handler for Click event.
+ *
+ * @param {Object} e Event object.
+ * @this Gridmap
+ */
+const defaultOnClick = function (e) {
+    const activeShape = e.originalEvent.activeObject;
+
+    this.polygonActive = new ymaps.Polygon([activeShape._properties.objectGeometry], {
+        hintContent: 'Polygon'
+    }, {
+        fillColor: '#00FF0088',
+        strokeWidth: 3
+    });
+
+    this._options.map.geoObjects.add(this.polygonActive);
+};
+
+export default defaultOnClick;
